8e49720236177c15120667b395734fa5bd841914
FrauH0lle <roland.goers@posteo.de>
2026-05-04 13:22:30 +0200

    gptel-openai-responses: Fix reasoning stream closing

--- gptel.el
+++ gptel.el
@@ -1955,7 +1955,7 @@ for streaming responses only."
              (start-marker (plist-get info :position)))
         (with-current-buffer (marker-buffer start-marker)
           (if (eq text t)               ;end of stream
-              (progn
+              (when (plist-get info :reasoning-open)
                 (gptel-curl--stream-insert-response
                  (concat (if (derived-mode-p 'org-mode)
                              "\n#+end_reasoning"
@@ -1964,6 +1964,7 @@ for streaming responses only."
                                        'keymap gptel--markdown-block-map))
                          gptel-response-separator)
                  info t)
+                (plist-put info :reasoning-open nil)
                 (ignore-errors          ;fold block
                   (save-excursion
                     (goto-char tracking-marker)
@@ -1987,7 +1988,8 @@ for streaming responses only."
                            ;; TODO(reasoning) remove properties and strip instead
                            (propertize "``` reasoning\n" 'gptel 'ignore
                                        'keymap gptel--markdown-block-map)))
-                 info t)))
+                 info t)
+                (plist-put info :reasoning-open t)))
             (if (eq include 'ignore)
                 (progn
                   (add-text-properties
